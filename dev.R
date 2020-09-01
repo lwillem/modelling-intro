3cdddaccf378322fc29c7fe048ca19597fbb78ce
lwillem <lander.willem@uantwerpen.be>
2020-09-01 16:10:04 +0200

    Updated R and SIR intro, and added html pages to master branch

--- dev.R
+++ dev.R
@@ -1,17 +1,10 @@
 require('rmarkdown')
 
-project_dir <- path.expand("~/Documents/university/teaching/GitHub_course_repos/modelling_course")
+project_dir <- path.expand(".")
 Rmd_dir <- file.path(project_dir, "Rmd")
-html_dir <- file.path(project_dir, "website")
+html_dir <- file.path(project_dir, "gh-pages")
 
-# Rmd_files <- c("index","code_sto_model","play_with_seitl","play_with_seitl_example")
-# Rmd_files <- c("code_sto_model", "SEITL_model", "SEITL_deter", "SEITL_flowchart")
-# Rmd_files <- c("index") #,"intro_to_r_gk","code_sto_model","SEITL_implement_partial", "SEITL_implement_full")
-# Rmd_files <- c("index","intro_to_r_gk", "code_sto_model", "SEITL_model", "SEITL_deter", "SEITL_flowchart", "SEITL_implement_partial", "SEITL_implement_full", "SEITL_transition_table") 
-# Rmd_files <- c("SEITL_transition_table") #my_first_fitmodel
-# Rmd_files <- c("index","using_fitR","posterior_example_solution","posterior_example")
 Rmd_files <- c("index","intro_to_r_lw", "intro_to_SIR_lw") 
-
 
 for (Rmd_file in Rmd_files) {
   render(file.path(Rmd_dir, sprintf("%s.Rmd", Rmd_file)),
