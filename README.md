4c7246bea89a2b0e3f768ca9455a6372ada4e9b4
lwillem <lander.willem@uantwerpen.be>
2020-09-01 12:12:35 +0200

    Initial commit with changes for the course in Antwerp, 2020.

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Infectious disease modelling in R
 
-Introductory material based on a course on infectious disease modelling held in Chennai, India, 23-26 November 2015.
+Introductory material for the summerschool on "Modelling Infectious Diseases and Health Economics" at the University of Antwerp from 7-11th September, 2020. This summerschool is a collaboration between the University of Antwerp, Hasselt University and the Institute of Tropical Medicine in Antwerp, Belgium.
 
-For the course materials, navigate to the [course webpage](http://sbfnk.github.io/modelling).
+For the course materials, navigate to the [course webpage](http://lwillem.github.io/modelling_course).
+
+We started from introductory material based on a course on infectious disease modelling by held in Chennai, India, 23-26 November 2015 by Sebastian Funk, Gwen Knight and Anton Camacho. We fully acknowledge their material and thank them for making them open-source via [GitHub](https://github.com/sbfnk/modelling).
+
+
--- dev.R
+++ dev.R
@@ -7,13 +7,14 @@ html_dir <- file.path(project_dir, "website")
 # Rmd_files <- c("index","code_sto_model","play_with_seitl","play_with_seitl_example")
 # Rmd_files <- c("code_sto_model", "SEITL_model", "SEITL_deter", "SEITL_flowchart")
 # Rmd_files <- c("index") #,"intro_to_r_gk","code_sto_model","SEITL_implement_partial", "SEITL_implement_full")
-Rmd_files <- c("index","intro_to_r_gk", "code_sto_model", "SEITL_model", "SEITL_deter", "SEITL_flowchart", "SEITL_implement_partial", "SEITL_implement_full", "SEITL_transition_table") 
+# Rmd_files <- c("index","intro_to_r_gk", "code_sto_model", "SEITL_model", "SEITL_deter", "SEITL_flowchart", "SEITL_implement_partial", "SEITL_implement_full", "SEITL_transition_table") 
 # Rmd_files <- c("SEITL_transition_table") #my_first_fitmodel
 # Rmd_files <- c("index","using_fitR","posterior_example_solution","posterior_example")
+Rmd_files <- c("index","intro_to_r_lw", "intro_to_SIR_lw") 
 
 
 for (Rmd_file in Rmd_files) {
-	render(file.path(Rmd_dir, sprintf("%s.Rmd", Rmd_file)),
-		output_dir = html_dir)
+  render(file.path(Rmd_dir, sprintf("%s.Rmd", Rmd_file)),
+         output_dir = html_dir)
 }
 
