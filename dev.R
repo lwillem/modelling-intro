77341f07120a8b3b25b12216165ec9d6e1b393c1
lwillem <lander.willem@uantwerpen.be>
2020-09-01 16:14:04 +0200

    Uploaded html files into "docs" folder for the GitHub Pages.

--- dev.R
+++ dev.R
@@ -2,7 +2,7 @@ require('rmarkdown')
 
 project_dir <- path.expand(".")
 Rmd_dir <- file.path(project_dir, "Rmd")
-html_dir <- file.path(project_dir, "gh-pages")
+html_dir <- file.path(project_dir, "docs")
 
 Rmd_files <- c("index","intro_to_r_lw", "intro_to_SIR_lw") 
 
