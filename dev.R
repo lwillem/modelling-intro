642289829d5b45c4cb86ba0375e24cbd76e48149
lwillem <lander.willem@uantwerpen.be>
2020-09-08 17:13:45 +0200

    Renamed files and updated the index page.

--- dev.R
+++ dev.R
@@ -4,9 +4,9 @@ project_dir <- path.expand(".")
 Rmd_dir <- file.path(project_dir, "Rmd")
 html_dir <- file.path(project_dir, "docs")
 
-Rmd_files <- c("index","intro_to_r_lw", "intro_to_SIR_lw",
+Rmd_files <- c("index","intro_to_r", "intro_to_SIR",
                "intro_to_RStudio",'intro_to_IBM_coding',
-               'intro_to_IBM_scenario','intro_to_IBM_household') 
+               'intro_to_IBM_walk','intro_to_IBM_location') 
 
 for (Rmd_file in Rmd_files) {
   render(file.path(Rmd_dir, sprintf("%s.Rmd", Rmd_file)),
