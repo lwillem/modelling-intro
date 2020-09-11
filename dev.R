8cd8ae6824dbd1fca9ff0aee9a1504bd19ade0a9
lwillem <lander.willem@uantwerpen.be>
2020-09-11 09:47:21 +0200

    Included demo on HTA uncertainty

--- dev.R
+++ dev.R
@@ -7,7 +7,8 @@ html_dir <- file.path(project_dir, "docs")
 Rmd_files <- c("index","intro_to_r", "intro_to_SIR",
                "intro_to_RStudio",'intro_to_IBM_coding',
                'intro_to_IBM_walk','intro_to_IBM_location',
-               'HTA_flu_basic','HTA_flu_CEAresults') 
+               'HTA_flu_basic','HTA_flu_CEAresults',
+               'HTA_uncertainty') 
 
 for (Rmd_file in Rmd_files) {
   render(file.path(Rmd_dir, sprintf("%s.Rmd", Rmd_file)),
