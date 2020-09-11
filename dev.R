bed8284bd6719c29e9ca7df78c32b915233268ca
lwillem <lander.willem@uantwerpen.be>
2020-09-11 10:27:21 +0200

    Included EVPPI tutorial.

--- dev.R
+++ dev.R
@@ -8,7 +8,7 @@ Rmd_files <- c("index","intro_to_r", "intro_to_SIR",
                "intro_to_RStudio",'intro_to_IBM_coding',
                'intro_to_IBM_walk','intro_to_IBM_location',
                'HTA_flu_basic','HTA_flu_CEAresults',
-               'HTA_uncertainty') 
+               'HTA_flu_uncertainty','HTA_flu_evppi') 
 
 for (Rmd_file in Rmd_files) {
   render(file.path(Rmd_dir, sprintf("%s.Rmd", Rmd_file)),
